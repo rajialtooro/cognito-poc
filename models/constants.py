a491c03f41e65b26e28ae1f07b0db65ceb48d1bc
Mo. Mitwaly <devlead@altooro.com>
2021-07-13 12:23:58 +0300

    Fix capitalization bug with GetLength, Fix Python main exercises

--- models/constants.py
+++ models/constants.py
@@ -3,7 +3,7 @@
 # * when adding more items to an array, simply put the correct syntax
 # * creating a new array for a language is in the following naming convention: correct_LANGNAME_patterns
 correct_patterns = {
-    "cs": ["Console.WriteLine", "Console.Write","WriteLine", ".Write", ".ReadLine", "int.Parse", ".Length", "Char.Parse", "Boolean.Parse"],
+    "cs": ["Console.WriteLine", "Console.Write","WriteLine", ".Write", ".ReadLine", "int.Parse", r"\.Length", r"\.GetLength" ,"Char.Parse", "Boolean.Parse"],
     "java": ["Scanner(System.in)", "String", "System.in", ".nextInt", "Integer.parseInt", "Boolean.parseBoolean",
              ".toString", ".valueOf", "System."],
     "js": ["console.log"],
