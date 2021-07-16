02aadac16b29979fbfb920f46838190871f99a22
unknown <julian.azzam236@gmail.com>
2021-07-16 13:06:05 +0300

    add adjustments to assignment solution orch to match the new assessment analytics

--- models/AssignmentData.py
+++ models/AssignmentData.py
@@ -6,10 +6,10 @@ class UserChallengeData(BaseModel):
     time_spent: int
     coding_time: int
     time_out_tab: int
-    error: str
+    error: str   
 
 class AssignmentData(BaseModel):
     course_id: str
     assignment_id: str
     user_id: str
-    challenges_data: list[UserChallengeData]
+    challenges_data: dict
